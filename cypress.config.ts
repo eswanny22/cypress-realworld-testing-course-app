28be7fc7a13acc2d0009df7c9c5ba1f0190527fd
Eric Swanson <eric@cypress.io>
2022-08-30 12:31:10 -0500

    try again

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,14 +10,18 @@
 
 
 import { defineConfig } from 'cypress'
-
+const del = require('del')
 export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
       on('after:spec', (spec, results) => {
-        /* ... */
+        if (results && results.stats.failures === 0 && results.video) {
+          // `del()` returns a promise, so it's important to return it to ensure
+          // deleting the video is finished before moving on
+          return del(results.video)
+        }
       })
     }
   },
