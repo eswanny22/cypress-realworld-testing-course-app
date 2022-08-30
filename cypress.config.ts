ad58b04b3d2aa7e679dee1d9f2bed02b3cf2de10
Eric Swanson <eric@cypress.io>
2022-08-30 12:16:39 -0500

    was my config broken

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,9 @@
-import { defineConfig } from 'cypress'
-
-// need to install the "del" module as a dependency
-// npm i del --save-dev
-const del = require('del')
-
+import { defineConfig } from "cypress";
 export default defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      on('after:spec', (spec, results) => {
-        if (results && results.stats.failures === 0 && results.video) {
-          // `del()` returns a promise, so it's important to return it to ensure
-          // deleting the video is finished before moving on
-          return del(results.video)
-        }
-      })
-    }
+      // implement node event listeners here
+    },
   },
   projectId: "jwt3sm"
-})
+});
