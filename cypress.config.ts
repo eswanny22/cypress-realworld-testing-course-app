f7c21ce699af3a4aaeb7136c3c88221a26b7361a
Eric Swanson <eric@cypress.io>
2022-08-30 11:50:57 -0500

    delete recorded video for specs with no failing tests + videoUploadOnPasses: false

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('after:spec', (spec, results) => {
+        if (results && results.stats.failures === 0 && results.video) {
+          // `del()` returns a promise, so it's important to return it to ensure
+          // deleting the video is finished before moving on
+          return del(results.video)
+        }
+      })
     },
   },
   projectId: "jwt3sm",
